fed48359626a1918664559936deb210cfb8bf6e9
hh2110 <hh2110@icloud.com>
2019-01-15 10:55:06 +0000

    reading in data from csv and dropping unwanted columns

--- AddressSorter.py
+++ AddressSorter.py
@@ -1,5 +1,17 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from gmplot import gmplot
 
-print('hi')
+# read in csv as df (with the first column as index_col)
+data = pd.read_csv("Jan19_SH.csv", index_col=0)
+
+# remove unwanted columns
+unwanted=['opdhVbp', 'vstId', 'vstDate', 'opdhVpulse',
+          'opdhVtemp', 'opdhDhtn', 'opdhDihd', 'opdhDhyper']
+data.drop(unwanted, inplace=True, axis=1)
+# inplace=true tells pd to make the changes in our object
+# axis=1 - dropping columns
+
+print('number of rows in df ', len(data))
