8ac5f9599110eb8175284ac3ff70ae64931fe228
hh2110 <hh2110@icloud.com>
2019-01-16 11:54:06 +0000

    moved the data cleaning lines to ipynb

--- InitialGoogleMapsAddressSearch.py
+++ InitialGoogleMapsAddressSearch.py
@@ -10,24 +10,6 @@ import random
 # read in csv as df (with the first column as index_col)
 data = pd.read_csv("./data/Jan19_SH.csv", index_col=0)
 
-# remove unwanted columns
-unwanted=['opdhVbp', 'vstId', 'vstDate', 'opdhVpulse',
-          'opdhVtemp', 'opdhDhtn', 'opdhDihd', 'opdhDhyper',
-          'pntAge', 'opdhVbmi', 'opdhVheight', 'opdhVweight',
-          'opdhDdm', 'vstAmount', 'vsrcId']
-data.drop(unwanted, inplace=True, axis=1)
-# inplace=true tells pd to make the changes in our object
-# axis=1 - dropping columns
-
-# check for empty cells and replace with 'Pakistan' (default)
-print('number of empty cells', data.isnull().sum())
-data['pntAddress']=data['pntAddress'].fillna('Pakistan')
-print('number of empty cells after fillna', data.isnull().sum(), '\n')
-
-# check the types of data in the addresses column
-print('type of data in the df ', data.dtypes)
-print('shape of dataFrame ', np.shape(data), '\n')
-
 # need to use googlemaps to find lat and loong of addresses
 with open('api.txt') as f:
     myAPIkey=f.readline()
