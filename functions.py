d6b4163dde32a0862ffaa4edf5c72cfbc07a0eaa
hh2110 <hh2110@icloud.com>
2019-01-16 10:19:58 +0000

    new function to compare individual words from two strings

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import Levenshtein as lev
+
 # printing the geocode object
 def printGeocodeResult(geoc):
     for k in geoc:
@@ -15,3 +17,34 @@ def checkPartialMatch(geoc):
                       check address', j)
                 return False
     return 
+
+# levenshtein distance funtion
+# to measure difference between two strings
+def SimilarityBtwStrings(inp, res):
+    # only consider whole of input but part of result - first 2 elements
+    
+    # first get a list of result words
+    rWordList=[]
+    #list1 is based on commas
+    list1=res.split(', ')[0:2] #this number 2 gets us the first 2 elements
+    for rPhrase in list1:
+        if ' ' in rPhrase:
+            list2=rPhrase.split(' ')
+            for rWord in list2:
+                rWordList.append(rWord)
+        else:
+            rWordList.append(rPhrase)
+    rWordList=[i.lower() for i in rWordList]
+    
+    iWordList=inp.strip().split(' ')
+    iWordList=[i.lower() for i in iWordList]
+
+    for i in iWordList:
+        for r in rWordList:
+            if r!='peshawar' and i!='peshawar' and \
+               r!='phase' and i!='phase' and \
+               r.isdigit()!=True and i.isdigit()!=True:
+                   if lev.distance(i,r) < 2:
+                       return 1
+      
+    return 0
